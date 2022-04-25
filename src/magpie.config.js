025d7f0afa2760509ae567398ee215edbabf8464
michael-franke <the.secret.goldfish@me.com>
2022-04-25 16:33:08 +0200

    deploy information w/ new backend

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,10 +1,10 @@
 export default {
-  experimentId: '6',
-  serverUrl: 'https://magpie-refactored-2.herokuapp.com/',
-  socketUrl: 'wss://magpie-refactored-2.herokuapp.com/socket',
+  experimentId: '85',
+  serverUrl: 'https://mcmpact.ikw.uni-osnabrueck.de/magpie/',
+  socketUrl: 'wss://mcmpact.ikw.uni-osnabrueck.de/magpie/socket',
   // this will be used in prolific mode
   completionUrl: 'https://...',
-  contactEmail: 'test@random.com',
+  contactEmail: 'mchfranke@gmail.com',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'debug'
+  mode: 'directLink'
 };
